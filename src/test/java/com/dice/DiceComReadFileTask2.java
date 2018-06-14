72f6d91667e45f37c81c3921b57ed46e48b8a524
hdanaci <mr.danaci@yahoo.com>
2018-06-14 11:53:15 -0500

    add task1-2

---
+++ src/test/java/com/dice/DiceComReadFileTask2.java
@@ -0,0 +1,62 @@
+package com.dice;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DiceComReadFileTask2 {
+	public static List<String> keywordList = new ArrayList<>();
+	public static List<String> zipCodeList = new ArrayList<>();
+	public static List<String> jobCount = new ArrayList<>();
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+
+		try (FileReader fr = new FileReader("Keywords.txt"); BufferedReader br = new BufferedReader(fr);) {
+
+			String value = null;
+			while ((value = br.readLine()) != null) {
+				keywordList.add(value);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try (FileReader fr = new FileReader("ZipList.txt"); BufferedReader br = new BufferedReader(fr);) {
+
+			String value = null;
+			while ((value = br.readLine()) != null) {
+				zipCodeList.add(value);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < keywordList.size(); i++) {
+			WebDriver driver = new ChromeDriver();
+			DiceComTask1.checkKeyword(driver, keywordList.get(i), zipCodeList.get(i));
+			jobCount.add(DiceComTask1.checkCount(driver, keywordList.get(i), zipCodeList.get(i)));
+			driver.quit();
+		}
+
+		for (String each : jobCount) {
+
+			System.out.println(each);
+		}
+
+	}
+
+}
---
+++ src/test/java/com/dice/DiceComTask1.java
@@ -0,0 +1,109 @@
+package com.dice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * @author bravo 1)Create arraylist of keywords. add 20 different keyworks
+ *         list.add("java"); pass each item to search box and print accordingly.
+ *         modify your arraylist
+ * 
+ *         java-1234
+ * 
+ *         2) Store all keywords into a text file read the text file and repeat
+ *         above steps.
+ * 
+ *         store keyword and results count into an arraylist. ----
+ * 
+ *         after closing browser. print contents of arraylist that was updated
+ *         each time we looped.
+ * 
+ *         commit > push > share your github link
+ */
+public class DiceComTask1 {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		
+		String[] list = { "Junior Developer", "Java Developer", "Senior Business Analyst", "Senior Software Engineer",
+				"Software Engineer", "system Administrator", "Selenium Automation Engineer",
+				"Selenium Mobile Web service Tester", "Selenium WebDriver", "Ruby Rails Developer", "RF Engineer",
+				"Test Engineer", "Computer Operator", "Designer", "Linux Administrator", "Lead Business Analyst",
+				"Marketing Manager", "Network Administrator", "Network Engineer", "Business Analyst Project Manager" };
+
+		List<String> keywords = new ArrayList<>();
+		for (int i = 0; i < list.length; i++) {
+			keywords.add(list[i]);
+		}
+		
+		String[] zips = { "90001", "80001", "32004", "60001", "48001", "10001", "43001", "73301", "20040", "98001",
+				"15001", "37010", "27006", "25813", "55001", "85001", "66002", "46001", "96701", "35004" };
+		List<String> zipCode = new ArrayList<>();
+		for (int i = 0; i < list.length; i++) {
+			zipCode.add(zips[i]);
+		}
+
+		List<String> jobCount = new ArrayList<>();
+
+		for (int i = 0; i < zipCode.size(); i++) {
+			WebDriver driver = new ChromeDriver();
+			checkKeyword(driver, keywords.get(i), zipCode.get(i));
+			jobCount.add(checkCount(driver, keywords.get(i), zipCode.get(i)));
+			driver.close();
+		}
+		System.out.println(jobCount);
+
+	}
+
+	public static void checkKeyword(WebDriver driver2, String key, String zip) {
+
+		driver2.manage().window().fullscreen();
+		driver2.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		
+		String url = "https://dice.com";
+		driver2.get(url);
+
+		String actualTitle = driver2.getTitle();
+		String expectedTitle = "Job Search for Technology Professionals | Dice.com";
+
+		if (actualTitle.equals(expectedTitle)) {
+			System.out.println("Step PASS. Dice homepage successfully loaded");
+		} else {
+			System.out.println("Step FAIL. Dice homepage did not load successfully");
+			throw new RuntimeException("Step FAIL. Dice homepage did not load successfully");
+		}
+
+		String keyword = key;
+		driver2.findElement(By.id("search-field-keyword")).clear();
+		driver2.findElement(By.id("search-field-keyword")).sendKeys(keyword);
+
+		String location = zip;
+		driver2.findElement(By.id("search-field-location")).clear();
+		driver2.findElement(By.id("search-field-location")).sendKeys(location);
+
+		driver2.findElement(By.id("findTechJobs")).click();
+
+	}
+
+	public static String checkCount(WebDriver driver2, String key, String zip) {
+		String count = driver2.findElement(By.id("posiCountId")).getText();
+		System.out.println(count);
+
+		int countResult = Integer.parseInt(count.replaceAll(",", ""));
+
+		if (countResult > 0) {
+			return "Keyword : " + key + " search returned : " + countResult + " results in " + zip;
+		} else {
+			return "Step FAIL : Keyword : " + key + " search returned : " + countResult + " results in " + zip;
+		}
+		
+
+	}
+
+}
